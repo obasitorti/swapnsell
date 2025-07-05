543b3bc7a99bbaf65deb148e4dcd44448dc1beca
Obasi <objtorti@hotmail.com>
2025-07-05 10:45:25 +0100

    created and activated some functionality for the website

--- project/swapandsell/urls.py
+++ project/swapandsell/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('allgadgets/', views.all_gadgets, name='all_gadgets'),
     path('addproduct/', views.add_product, name='add_product'),
+    path('allgadgets/<int:detail_id>/', views.detail, name='detail'),
+    path('mygadgets/', views.my_gadgets, name='my_gadgets'),
+    path('editproduct/<int:detail_id>/', views.edit_product, name='edit_product'),
 ]
--- project/swapandsell/views.py
+++ project/swapandsell/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Product
+from .forms import ProductForm
+from django.shortcuts import redirect
 
 def index(request):
     return render(request, 'swapandsell/index.html')
@@ -9,4 +11,36 @@ def all_gadgets(request):
     all_gadgets = Product.objects.order_by('-listing_date')
     context = {'all_gadgets': all_gadgets}
     return render(request, 'swapandsell/all_gadgets.html', context)
-# Create your views here.
+
+def add_product(request):
+    if request.method != 'POST':
+        form = ProductForm()
+    else:
+        form = ProductForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('swapandsell:all_gadgets')
+    context = {'form': form}
+    return render(request, 'swapandsell/add_product.html', context)
+
+def detail(request, detail_id):
+    gadget = Product.objects.get(id=detail_id)
+    context = {'gadget': gadget}
+    return render(request, 'swapandsell/detail.html', context)
+
+def my_gadgets(request):
+    my_gadgets = Product.objects.order_by('-listing_date')
+    context = {'all_gadgets': my_gadgets}
+    return render(request, 'swapandsell/mygadgets.html', context)
+
+def edit_product(request, detail_id):
+    gadget = Product.objects.get(id=detail_id)
+    if request.method != 'POST':
+        form = ProductForm(instance=gadget)
+    else:
+        form = ProductForm(instance=gadget, data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('swapandsell:all_gadgets')
+    context = {'form': form, 'gadget': gadget}
+    return render(request, 'swapandsell/edit_product.html', context)
