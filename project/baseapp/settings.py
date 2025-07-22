c3f4b94ce62b270abceb1f89e9b854a3aba2ad49
obasitorti <objtorti@hotmail.com>
2025-07-22 11:14:02 +0100

    changed debug to false to prepare for deployment

--- project/baseapp/settings.py
+++ project/baseapp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(ru*)@awms6^qa^ju2ru(!9v^3v@qi#5js0w*=_q^k(mgw0zs!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -155,3 +156,4 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 
+django_heroku.settings(locals())
