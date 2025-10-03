c7810dcf1d93984d5e61c9677ae1f1e0febf2f1c
obasitorti <objtorti@hotmail.com>
2025-10-03 09:56:00 +0100

    Auto-login user after registration
    After a successful registration, the new user is now automatically logged in before being redirected to the index page. This improves user experience by eliminating the need for a separate login step after registration.

--- users/views.py
+++ users/views.py
@@ -1,19 +1,24 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout as django_logout
 from .forms import CreateUserForm
+from django.contrib.auth import login
 
 def logout_view(request):
 
     django_logout(request)
     return redirect('swapandsell:index')
 
+
+
 def register_view(request):
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('swapandsell:index')
+            user = form.save()  
+            login(request, user) 
+            return redirect('swapandsell:index') 
     else:
         form = CreateUserForm()
     
     return render(request, 'registration/register.html', {'form': form})
+
